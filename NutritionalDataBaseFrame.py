ce8fd6404cebec29f59ce3c9f15055c5f1a3bb4b
DaicosJ <daicoz.j@gmail.com>
2024-10-06 17:55:42 +1000

    Python code changes

---
+++ NutritionalDataBaseFrame.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+import gettext
+_ = gettext.gettext
+
+###########################################################################
+## Class MainFrame
+###########################################################################
+
+class MainFrame ( wx.Frame ):
+
+    def __init__( self, parent ):
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = _(u"Nutritional Database App"), pos = wx.DefaultPosition, size = wx.Size( 582,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+        self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+        gSizer2 = wx.GridSizer( 0, 2, 0, 0 )
+
+        self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, _(u"Protein (g):"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText1.Wrap( -1 )
+
+        gSizer2.Add( self.m_staticText1, 0, wx.ALL, 5 )
+
+        self.m_textCtrl1 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        gSizer2.Add( self.m_textCtrl1, 0, wx.ALL, 5 )
+
+        self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, _(u"Fat (g):"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText2.Wrap( -1 )
+
+        gSizer2.Add( self.m_staticText2, 0, wx.ALL, 5 )
+
+        self.m_textCtrl2 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        gSizer2.Add( self.m_textCtrl2, 0, wx.ALL, 5 )
+
+        self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, _(u"Carbohydrates (g):"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText3.Wrap( -1 )
+
+        gSizer2.Add( self.m_staticText3, 0, wx.ALL, 5 )
+
+        self.m_textCtrl3 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        gSizer2.Add( self.m_textCtrl3, 0, wx.ALL, 5 )
+
+        self.m_staticText4 = wx.StaticText( self, wx.ID_ANY, _(u"Calories:"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText4.Wrap( -1 )
+
+        gSizer2.Add( self.m_staticText4, 0, wx.ALL, 5 )
+
+        self.m_textCtrl4 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        gSizer2.Add( self.m_textCtrl4, 0, wx.ALL, 5 )
+
+        bSizer5 = wx.BoxSizer( wx.HORIZONTAL )
+
+        bSizer5.SetMinSize( wx.Size( -3,-3 ) )
+        self.m_staticText5 = wx.StaticText( self, wx.ID_ANY, _(u"Protein Range (g):"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText5.Wrap( -1 )
+
+        bSizer5.Add( self.m_staticText5, 0, wx.ALL, 5 )
+
+        self.m_textCtrl5 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer5.Add( self.m_textCtrl5, 0, wx.ALL, 5 )
+
+        self.m_textCtrl6 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer5.Add( self.m_textCtrl6, 0, wx.ALL, 5 )
+
+
+        gSizer2.Add( bSizer5, 1, wx.EXPAND, 5 )
+
+        bSizer51 = wx.BoxSizer( wx.HORIZONTAL )
+
+        bSizer51.SetMinSize( wx.Size( -3,-3 ) )
+        self.m_staticText6 = wx.StaticText( self, wx.ID_ANY, _(u"Fat Range (g):"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText6.Wrap( -1 )
+
+        bSizer51.Add( self.m_staticText6, 0, wx.ALL, 5 )
+
+        self.m_textCtrl7 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer51.Add( self.m_textCtrl7, 0, wx.ALL, 5 )
+
+        self.m_textCtrl8 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer51.Add( self.m_textCtrl8, 0, wx.ALL, 5 )
+
+
+        gSizer2.Add( bSizer51, 1, wx.EXPAND, 5 )
+
+        bSizer52 = wx.BoxSizer( wx.HORIZONTAL )
+
+        bSizer52.SetMinSize( wx.Size( -3,-3 ) )
+        self.m_staticText7 = wx.StaticText( self, wx.ID_ANY, _(u"Protein Range (g):"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText7.Wrap( -1 )
+
+        bSizer52.Add( self.m_staticText7, 0, wx.ALL, 5 )
+
+        self.m_textCtrl9 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer52.Add( self.m_textCtrl9, 0, wx.ALL, 5 )
+
+        self.m_textCtrl10 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer52.Add( self.m_textCtrl10, 0, wx.ALL, 5 )
+
+
+        gSizer2.Add( bSizer52, 1, wx.EXPAND, 5 )
+
+        bSizer53 = wx.BoxSizer( wx.HORIZONTAL )
+
+        bSizer53.SetMinSize( wx.Size( -3,-3 ) )
+        self.m_staticText8 = wx.StaticText( self, wx.ID_ANY, _(u"Calories Range (g):"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText8.Wrap( -1 )
+
+        bSizer53.Add( self.m_staticText8, 0, wx.ALL, 5 )
+
+        self.m_textCtrl11 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer53.Add( self.m_textCtrl11, 0, wx.ALL, 5 )
+
+        self.m_textCtrl12 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer53.Add( self.m_textCtrl12, 0, wx.ALL, 5 )
+
+
+        gSizer2.Add( bSizer53, 1, wx.EXPAND, 5 )
+
+        self.m_button1 = wx.Button( self, wx.ID_ANY, _(u"Search"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        gSizer2.Add( self.m_button1, 0, wx.ALL, 5 )
+
+        self.m_button2 = wx.Button( self, wx.ID_ANY, _(u"Reset"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        gSizer2.Add( self.m_button2, 0, wx.ALL, 5 )
+
+        self.m_staticText9 = wx.StaticText( self, wx.ID_ANY, _(u"Results"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText9.Wrap( -1 )
+
+        gSizer2.Add( self.m_staticText9, 0, wx.ALL, 5 )
+
+        self.m_textCtrl13 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        gSizer2.Add( self.m_textCtrl13, 0, wx.ALL, 5 )
+
+
+        self.SetSizer( gSizer2 )
+        self.Layout()
+
+        self.Centre( wx.BOTH )
+
+        # Connect Events
+        self.m_button1.Bind( wx.EVT_BUTTON, self.on_search )
+        self.m_button2.Bind( wx.EVT_BUTTON, self.on_reset )
+
+    def __del__( self ):
+        pass
+
+
+    # Virtual event handlers, override them in your derived class
+    def on_search( self, event ):
+        event.Skip()
+
+    def on_reset( self, event ):
+        event.Skip()
+
+
--- run_app.py
+++ run_app.py
@@ -2,28 +2,25 @@ import wx
 import pandas as pd
 from NutritionalDataBaseFrame import MainFrame
 
-
-def load_dataset(file_path='Food_Nutrition_Dataset.csv'):
+def load_dataset(file_path='.\Food_Nutrition_Dataset.csv'):
+    df = pd.read_csv(file_path)
     return pd.read_csv(file_path)
 
-
 def search_nutritional_values(df, protein=None, fat=None, carbs=None, calories=None):
-    filtered_df = df
-    if protein:
-        filtered_df = filtered_df[filtered_df['Protein (g)'] >= protein]
-    if fat:
-        filtered_df = filtered_df[filtered_df['Fat (g)'] >= fat]
-    if carbs:
-        filtered_df = filtered_df[filtered_df['Carbohydrates (g)'] >= carbs]
-    if calories:
-        filtered_df = filtered_df[filtered_df['Calories (g)'] >= calories]
-
+    filtered_df=df
+    if protein is not None:
+        filtered_df = filtered_df[filtered_df['Protein'] >= protein]
+    if fat is not None:
+        filtered_df = filtered_df[filtered_df['Fat'] >= fat]
+    if carbs is not None:
+        filtered_df = filtered_df[filtered_df['Carbohydrates'] >= carbs]
+    if calories is not None:
+        filtered_df = filtered_df[filtered_df['Caloric Value'] >= calories]
+    
     return filtered_df
-
 
 def reset_filters(df):
     return df
-
 
 class MyApp(wx.App):
     def OnInit(self):
@@ -33,27 +30,41 @@ class MyApp(wx.App):
 
         self.frame.m_button1.Bind(wx.EVT_BUTTON, self.on_search)
         self.frame.m_button2.Bind(wx.EVT_BUTTON, self.on_reset)
-
+        
         return True
-
+    
     def on_search(self, event):
         protein = self.frame.m_textCtrl1.GetValue()
         fat = self.frame.m_textCtrl2.GetValue()
         carbs = self.frame.m_textCtrl3.GetValue()
         calories = self.frame.m_textCtrl4.GetValue()
 
+        print(f"Input Values - Protein: {protein}, Fat: {fat}, Carbs: {carbs}, Calories: {calories}")
+
         try:
+            if protein == "" and fat == "" and carbs == "" and calories == "":
+                wx.MessageBox("Please Enter at least one nutritional calue to search.", 'Input Required', wx.OK | wx.ICON_WARNING)
+                return
+            
             protein = float(protein) if protein else None
             fat = float(fat) if fat else None
             carbs = float(carbs) if carbs else None
             calories = float(calories) if calories else None
 
-            filtered_data = search_nutritional_values(self.dataframe, protein, fat, carbs, calories)
-            print(filtered_data)
+            print(f"DataFrame Columns: {self.dataframe.columns}")
 
+            filtered_data = search_nutritional_values(self.dataframe, protein, fat, carbs, calories)
+            if filtered_data.empty:
+                wx.MessageBox("No results found", 'Info', wx.OK | wx.ICON_INFORMATION)
+            else:
+                results_str = filtered_data.to_string(index=False)
+                self.frame.m_textCtrl13.SetValue(results_str)    
+            
         except ValueError:
             wx.MessageBox('Please enter valid numerical values.', 'Error', wx.OK | wx.ICON_ERROR)
-
+        except Exception as e:
+            wx.MessageBox(f'An error occurred: {str(e)}', 'Error', wx.OK | wx.ICON_ERROR)
+    
     def on_reset(self, event):
         self.frame.m_textCtrl1.SetValue("")
         self.frame.m_textCtrl2.SetValue("")
@@ -61,7 +72,6 @@ class MyApp(wx.App):
         self.frame.m_textCtrl4.SetValue("")
         self.dataframe = reset_filters(self.dataframe)
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app = MyApp()
     app.MainLoop()
